f261f5b8878a5cbce676c306fa0aebc16b4b1208
Hu Yucheng <34648086+Willzy-x@users.noreply.github.com>
2018-06-05 10:14:42 +0800

    Update Logistic Regression with a Neural Network mindset.py

--- Logistic Regression with a Neural Network mindset.py
+++ Logistic Regression with a Neural Network mindset.py
@@ -29,3 +29,98 @@ num_px = train_set_x_orig.shape[1
 #There should be m_train (respectively m_test) columns.
 train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
 test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+#The main steps for building a Neural Network are:
+
+#Define the model structure (such as number of input features)
+#Initialize the model's parameters Loop:
+#   Calculate current loss (forward propagation)
+#   Calculate current gradient (backward propagation)
+#   Update parameters (gradient descent)
+                                
+def sigmoid(z):
+    s = 1/(1+(-1*np.exp(z)))
+    return s
+
+def initialize_with_zeros(dim):
+    w = np.zeros((dim,1))                            
+    b = 0
+    assert(w.shape == (dim,1))
+    assert(isinstance(b,float), or isinstance(b,int))
+                                
+    return w,b
+       
+#Implement a function propagate() that computes the cost function and its gradient.                                
+def propagate(w, b, X, Y):
+    m = X.shape[1]
+    
+    A = sigmoid(np.dot(w.T, X)+b)
+    cost = (-1/m)*(np.dot(Y, np.log(A))+np.dot(1-Y, np.log(1-A)))            
+    dw = (1/m)*(np.dot(X, (A-Y).T))
+    db = (1/m)*(np.sum(A-Y))   
+                                
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost 
+                                
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    costs = []
+    
+    for i in range(num_iterations):         
+        grads,cost = propagate(w, b, X, Y)
+        dw = grads["dw"]
+        db = grads["db"]
+        w = w - learning_rate*dw
+        b = b - learning_rate*db
+                                
+        if i % 100 == 0:
+            costs.append(cost)
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs     
+                                
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)                            
+                                
+    A= sigmoid(np.dot(w.T, X) + b)
+    
+    for i in range(A.shape[1]):
+        if(A[0, i] > 0.5):
+            Y_prediction[0, i] = 1
+        else:
+            Y_prediction[0, i] = 0         
+                                
+    assert(Y_prediction.shape == (1, m))
+    return Y_prediction
+                                
+def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):                                
+    """
+    Builds the logistic regression model by calling the function you've implemented previously
+    
+    Arguments:
+    X_train -- training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
+    Y_train -- training labels represented by a numpy array (vector) of shape (1, m_train)
+    X_test -- test set represented by a numpy array of shape (num_px * num_px * 3, m_test)
+    Y_test -- test labels represented by a numpy array (vector) of shape (1, m_test)
+    num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
+    learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
+    print_cost -- Set to true to print the cost every 100 iterations
+    
+    Returns:
+    d -- dictionary containing information about the model.
+    """          
