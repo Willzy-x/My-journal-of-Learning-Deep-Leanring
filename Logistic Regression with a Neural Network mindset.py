a8336106f296f601088e93bc41c289579d145b70
Hu Yucheng <34648086+Willzy-x@users.noreply.github.com>
2018-06-05 16:25:06 +0800

    Update Logistic Regression with a Neural Network mindset.py

--- Logistic Regression with a Neural Network mindset.py
+++ Logistic Regression with a Neural Network mindset.py
@@ -124,3 +124,31 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate
     Returns:
     d -- dictionary containing information about the model.
     """          
+    w, b = initialize_with_zeros(X_train.shape[0])
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost = False)
+    w = parameters["w"]
+    b = parameters["b"]                            
+                                
+    Y_prediction_test = predict(w,b,X_test)
+    Y_prediction_train = predict(w,b,X_train)                            
+                                
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))                            
+                                
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    
+    return d
+                                
+#Choice of learning rate
+#Reminder:
+#In order for Gradient Descent to work you must choose the learning rate wisely. 
+#The learning rate  αα  determines how rapidly we update the parameters. 
+#If the learning rate is too large we may "overshoot" the optimal value. Similarly, 
+#if it is too small we will need too many iterations to converge to the best values. 
+#That's why it is crucial to use a well-tuned learning rate.                                
