05e9a69c8f71558a15ee99a7867d40aca0145646
Hu Yucheng <34648086+Willzy-x@users.noreply.github.com>
2018-06-13 23:08:23 +0800

    Update Planar data classification with a hidden layer.py

--- Planar data classification with a hidden layer.py
+++ Planar data classification with a hidden layer.py
@@ -22,8 +22,12 @@ def layer_sizes(X, Y):
     n_y = Y.shape[0] # size of output layer
     return (n_x, n_h, n_y)
    
-def initialize_parameters(n_x, n_h, n_y):
-
+def initialize_parameters(n_x, n_h, n_y): #initialize the W1 and W2 randomly
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_h, n_y) * 0.01
+    b2 = np.zeros((n_y, 1))
+    
     assert (W1.shape == (n_h, n_x))
     assert (b1.shape == (n_h, 1))
     assert (W2.shape == (n_y, n_h))
@@ -37,9 +41,17 @@ def initialize_parameters(n_x, n_h, n_y):
    
    
 def forward_propagation(X, parameters):
-
-    assert(A2.shape == (1, X.shape[1]))
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
     
+    Z1 = np.dot(W1, X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2 
+    A2 = sigmoid(Z2)
+    
+    assert(A2.shape == (1, X.shape[1]))
     cache = {"Z1": Z1,
              "A1": A1,
              "Z2": Z2,
@@ -49,7 +61,9 @@ def forward_propagation(X, parameters):
    
    
 def compute_cost(A2, Y, parameters):
-
+    logprobs = np.multiply(np.log(A2),Y)
+    cost = (-1/m)* np.sum((logprobs + np.multiply(1-Y, np.log(1-A2))))      
+    
     cost = np.squeeze(cost)     # makes sure cost is the dimension we expect. 
                                 # E.g., turns [[17]] into 17 
     assert(isinstance(cost, float))
@@ -58,7 +72,18 @@ def compute_cost(A2, Y, parameters):
   
 
 def backward_propagation(parameters, cache, X, Y):
-
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+    
+    dZ2 = A2 - Y
+    dW2 = (1/m)*np.dot(dZ2, A1.T)
+    db2 = (1/m)*np.sum(dZ2, axis = 1, keepdims = True)
+    dZ1 = np.dot(W2.T, dZ2) * (1 - np.power(A1, 2))
+    dW1 = (1/m)*np.dot(dZ1, X.T)
+    db1 = (1/m)*np.sum(dZ1, axis = 1, keepdims = True)
+    
     grads = {"dW1": dW1,
              "db1": db1,
              "dW2": dW2,
@@ -67,7 +92,21 @@ def backward_propagation(parameters, cache, X, Y):
     return grads
     
 def update_parameters(parameters, grads, learning_rate = 1.2):
-
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    dW1 = grads["dW1"]
+    db1 = grads["db1"]
+    dW2 = grads["dW2"]
+    db2 = grads["db2"]
+    
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+    
     parameters = {"W1": W1,
                   "b1": b1,
                   "W2": W2,
@@ -76,10 +115,37 @@ def update_parameters(parameters, grads, learning_rate = 1.2):
     return parameters
     
 def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
-
+    np.random.seed(3)
+    n_x = layer_sizes(X, Y)[0]
+    n_y = layer_sizes(X, Y)[2]
+    
+    parameters = initialize_parameters(n_x, n_h, n_y)
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    for i in range(0, num_iterations):
+        # Forward propagation. Inputs: "X, parameters". Outputs: "A2, cache".
+        A2, cache = forward_propagation(X, parameters)
+        
+        # Cost function. Inputs: "A2, Y, parameters". Outputs: "cost".
+        cost = compute_cost(A2, Y, parameters)
+ 
+        # Backpropagation. Inputs: "parameters, cache, X, Y". Outputs: "grads".
+        grads = backward_propagation(parameters, cache, X, Y)
+ 
+        # Gradient descent parameter update. Inputs: "parameters, grads". Outputs: "parameters".
+        parameters = update_parameters(parameters, grads, learning_rate = 1.2)
+        
+        # Print the cost every 1000 iterations
+        if print_cost and i % 1000 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+            
     return parameters
 
 def predict(parameters, X):
-
+    A2, cache = forward_propagation(X, parameters)
+    predictions = (A2 > 0.5)
     return predictions
 
